875a2b3b3f6c4999ec8fe99186b17bf5757622cd
Nicholas Sng <nicholas_sng@ymail.com>
2024-06-12 20:47:09 +0800

    display category ticket page
    - err msg to chk max 2 tix, no tix from different cat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import EventDetailPage from './pages/EventDetailPage';
+import TicketPage from './pages/TicketPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
           <Route path="/event/:eventId" element={<EventDetailPage />} />
+          <Route path="/ticket/:eventId" element={<TicketPage />} />
         </Routes>
       </div>
     </Router>
