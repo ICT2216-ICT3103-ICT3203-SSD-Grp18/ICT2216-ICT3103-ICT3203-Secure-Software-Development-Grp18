2eba9b88abbf832f8d211e06ac47a76b31685477
Nicholas Sng <nicholas_sng@ymail.com>
2024-06-16 02:06:18 +0800

    add backend
    in root dir, just run npm start.

---
+++ backend/db.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+  console.log('Connected to database.');
+});
+
+module.exports = connection;
