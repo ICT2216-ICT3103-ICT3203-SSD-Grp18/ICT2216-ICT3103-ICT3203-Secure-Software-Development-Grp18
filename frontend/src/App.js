7d026587ae1b1fbcffe67f45194e444143da2caa
yurjere <68389411+yurjere@users.noreply.github.com>
2024-06-21 23:44:04 +0800

    done basic payment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import EventDetailPage from './pages/EventDetailPage';
 import TicketPage from './pages/TicketPage';
+import BuyerContactInformationPage from './pages/BuyerContactInformationPage';
+import PaymentPage from './pages/PaymentPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -14,6 +16,8 @@ const App = () => {
           <Route exact path="/" element={<LandingPage />} />
           <Route path="/event/:eventId" element={<EventDetailPage />} />
           <Route path="/ticket/:eventId" element={<TicketPage />} />
+          <Route path="/buyer-info" element={<BuyerContactInformationPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
         </Routes>
       </div>
     </Router>
